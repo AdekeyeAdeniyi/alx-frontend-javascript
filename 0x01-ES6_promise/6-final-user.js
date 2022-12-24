751c8705dc39f999c2665e65d78d20f276c46afa
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-12-24 01:14:42 +0100

    Func/Return arry when promise are all settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,17 @@
-import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((values) => {
-      const arr = [];
-      for (const item of values) {
-        arr.push({ status: item.status, value: item.value || item.reason });
-      }
-      return arr;
-    });
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
 }
 
 export default handleProfileSignup;
