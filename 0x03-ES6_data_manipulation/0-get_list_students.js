7cc00c383057ce665d535f9c6dcdabd34fb45c9d
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-04 22:30:45 +0100

    Func/Returns array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/* Function: Return an array of object */
+
+function getListStudents() {
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+}
+
+export default getListStudents;
