eeaa970da05e674311254edcdf3a97e5b90ac356
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-06 19:58:36 +0100

    Func/Return an array of object(s) with new grades

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,20 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable no-param-reassign */
 function updateStudentGradeByCity(data, location, newGrade) {
   if (Array.isArray(data) && location && Array.isArray(newGrade)) {
     const result = [...data].filter((item) => item.location === location);
 
-    [...result].map((data) => {
-      data.grades = 'N/A';
-      [...newGrade].forEach((el) => {
-        if (el.studentId === data.id) {
-          data.grades = el.grade;
+    const newData = [...result].map((data) => {
+      const student = data;
+      student.grades = 'N/A';
+      [...newGrade].forEach((grade) => {
+        if (grade.studentId === student.id) {
+          student.grades = grade.grade;
         }
       });
+
+      return student;
     });
 
-    return result;
+    return newData;
   }
 
   return [];
