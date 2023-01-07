10ddf8769f152a30610325a1d2463fa88dec1a44
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-07 02:20:05 +0100

    Create to addition functions

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,48 +1,38 @@
 interface DirectorInterface {
-  workFromHome: () => string,
-  getCoffeeBreak: () => string,
-  workDirectorTasks: () => string,
+  workFromHome: () => string;
+  getCoffeeBreak: () => string;
+  workDirectorTasks: () => string;
 }
 
 interface TeacherInterface {
-  workFromHome: () => string,
-  getCoffeeBreak: () => string,
-  workDirectorTasks: () => string,
+  workFromHome: () => string;
+  getCoffeeBreak: () => string;
+  workTeacherTasks: () => string;
 }
 
 class Director implements DirectorInterface {
-  workFromHome = (): string => {
-    return 'Working from home';
-  }
-  getCoffeeBreak = (): string => {
-    return 'Getting a coffee break';
-  };
+  workFromHome = (): string => 'Working from home'
 
-  workDirectorTasks = (): string => {
-    return 'Getting to director tasks';
-  }
+  getCoffeeBreak = (): string => 'Getting a coffee break';
+
+  workDirectorTasks = (): string => 'Getting to director tasks'
 }
 
-class Teacher  implements TeacherInterface {
-  workFromHome = (): string => {
-    return 'Cannot work from home';
-  }
-  getCoffeeBreak = (): string => {
-    return 'Cannot have a break';
-  };
+class Teacher implements TeacherInterface {
+  workFromHome = (): string => 'Cannot work from home'
 
-  workDirectorTasks = (): string => {
-    return 'Getting to work';
-  }
+  getCoffeeBreak = (): string => 'Cannot have a break';
+
+  workTeacherTasks = (): string => 'Getting to work'
 }
 
-const createEmployee = (salary: string | number): Director | Teacher =>{
-  if(typeof(salary) === 'number' && salary < 500){
+const createEmployee = (salary: string | number): Director | Teacher => {
+  if (typeof (salary) === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
-}
+};
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
+
+const executeWork = (employee: Teacher | Director): string => (employee instanceof Director ? employee.workDirectorTasks() : employee.workTeacherTasks());
